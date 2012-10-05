d6ce2198b4651f2f0db00927d8e88e0fc48b661e
joleques <joleques@gmail.com>
2012-10-04 21:08:04 -0300

    criado pacote estoria para armazenar um txt para nos fornecer a estoria para cada problema

--- src/rs/com/pedido/original/Pedido.java
+++ src/rs/com/pedido/original/Pedido.java
@@ -1,5 +1,7 @@
 package rs.com.pedido.original;
 
+/* solução para a estoria pedidos.txt*/
+
 public class Pedido {
 
 	private Long numero;
@@ -27,21 +29,28 @@ public class Pedido {
 		}
 		return null;
 	}
-
-	public void informarPorEmailCliente(){
-		if(status.equals("PR")){
-			enviarEmail("Enviando email para confirmar que o pedido foi realizado");
-		}else if(status.equals("PA")){
-			enviarEmail("Enviando email para confirmar o recebimento do pagamento do pedido");
-		}else if(status.equals("PE")){
-			enviarEmail("Enviando email informar que o pedido esta sendo preparado para ser entregue");
-		}else if(status.equals("PT")){
-			enviarEmail("Enviando email informar que já esta de posse da transportadora o pedido");
-		}else if(status.equals("PE")){
-			enviarEmail("Enviando email para confirmar a entrega do pedido");
+		
+	public void mudarStatus(String novoStatus) throws Exception{
+		if(novoStatus != null && !novoStatus.equals("")){
+			this.status = novoStatus;
+			if(status.equals("PR")){
+				enviarEmail("Enviando email para confirmar que o pedido foi realizado");
+			}else if(status.equals("PA")){
+				enviarEmail("Enviando email para confirmar o recebimento do pagamento do pedido");
+			}else if(status.equals("PE")){
+				enviarEmail("Enviando email informar que o pedido esta sendo preparado para ser entregue");
+			}else if(status.equals("PT")){
+				enviarEmail("Enviando email informar que já esta de posse da transportadora o pedido");
+			}else if(status.equals("PE")){
+				enviarEmail("Enviando email para confirmar a entrega do pedido");
+			}else{
+				throw new Exception("Tipo Passado Invalido.");
+			}
+		}else{
+			throw new Exception("Tipo Passado esta nulo ou vazio.");
 		}
 	}
-	
+
 	private void enviarEmail(String mensagem){
 		System.out.println(mensagem);
 	}
